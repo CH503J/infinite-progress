7b7d07465471b84f13dd2e484334dd435cc0020b
ChenJun <@CH503J>
2025-11-15 16:06:13 +0800

    feat(project): 初始化认证服务模块并优化项目结构

---
+++ ip-auth-service/src/main/java/com/ch503j/ipauthservice/IpAuthServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ch503j.ipauthservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IpAuthServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IpAuthServiceApplication.class, args);
+    }
+
+}
