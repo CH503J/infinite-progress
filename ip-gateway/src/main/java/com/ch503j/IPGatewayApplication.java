6cdabe0f4021c247fd77231d3e2430eb496285b5
ChenJun <@CH503J>
2025-11-15 13:35:43 +0800

    feat(project): 初始化项目结构和基础配置

---
+++ ip-gateway/src/main/java/com/ch503j/IPGatewayApplication.java
@@ -0,0 +1,17 @@
+package com.ch503j;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@Slf4j
+@SpringBootApplication
+@EnableDiscoveryClient
+public class IPGatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(IPGatewayApplication.class, args);
+        log.info("================= Gateway网关已启动 =================");
+    }
+}
